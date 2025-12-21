0423435304ae31b2d6ff44290d90fc8da1ef1e55
Pintu <pintukumar808284@gmail.com>
2025-12-21 18:04:34 +0530

    axiosinstance fix

--- frontend/src/service/axiosInstance.js
+++ frontend/src/service/axiosInstance.js
@@ -1,9 +1,17 @@
-import axios from "axios";
+"use client";
+// import axios from "axios";
 // import ENDPOINTS from "./endpoints";
 
-const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_REACT_APP_BASE_URL,
+import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_REACT_APP_BASE_URL;
+
+if (!baseURL) {
+  throw new Error("NEXT_PUBLIC_REACT_APP_BASE_URL is not defined");
+}
+
+const axiosInstance = axios.create({
+  baseURL,
   headers: {
     "Content-Type": "application/json",
 
